026e6de7bb8ad9d63c726434801e15d1df41f1dc
ST-Chara <74450074+ST-Chara@users.noreply.github.com>
2021-09-20 19:36:28 +0800

    Create king.cpp

---
+++ src/infcroya/classes/king.cpp
@@ -0,0 +1,42 @@
+#include "psycho.h"
+#include <game/server/entities/character.h>
+#include <game/server/player.h>
+#include <game/server/gamecontext.h>
+#include <game/server/entities/projectile.h>
+#include <game/server/entities/flag.h>
+#include <infcroya/entities/bouncing-bullet.h>
+
+CPsycho::CPsycho()
+{
+	CSkin skin;
+	skin.SetBodyColor(0, 0, 0);
+	skin.SetMarkingName("saddo");
+	skin.SetMarkingColor(0, 0, 0);
+	skin.SetDecorationName("default");
+	skin.SetDecorationColor(233, 158, 183);
+	skin.SetHandsColor(20, 208, 166);
+	skin.SetFeetColor(0,  0;
+	SetSkin(skin);
+	SetInfectedClass(ture);
+	SetName("Psycho");
+	Set06SkinName("Redboppenom");
+	Set06SkinColors(1363875, 655151);
+}
+
+void CPsycho::InitialWeaponsHealth(CCharacter* pChr)
+{
+	pChr->IncreaseHealth(10);
+	pChr->GiveWeapon(WEAPON_HAMMER, -1);
+	pChr->SetWeapon(WEAPON_HAMMER);
+	pChr->SetNormalEmote(EMOTE_NORMAL);
+}
+
+void CPsycho::OnWeaponFire(vec2 Direction, vec2 ProjStartPos, int Weapon, CCharacter* pChr)
+{
+	switch (Weapon) {
+		case WEAPON_HAMMER: {
+			HammerShoot(pChr, ProjStartPos);
+		} break;
+	}
+
+}
